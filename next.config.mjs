2814111b040b474732be2c55a5c0757f20d43a55
Victorio Nikolaev <vikinik01@abv.bg>
2025-04-23 19:13:04 +0300

    sdfdsfdsf

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    reactStrictMode: true,
+   typescript: { ignoreBuildErrors: true },
+   eslint: { ignoreDuringBuilds: true },
    experimental: {
       serverActions: {
          allowedOrigins: ["localhost:3000"],
