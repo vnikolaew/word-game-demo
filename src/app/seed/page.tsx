a021e70cb056e2d7b157696011c7fee291abfc8d
Victorio Nikolaev <vikinik01@abv.bg>
2025-04-23 19:23:59 +0300

    sdfdsfdsf

--- src/app/seed/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { main } from "../../../prisma/seed";
-
-async function Page() {
-   await main();
-   return <div>Seeded database.</div>;
-}
-
-export default Page;
