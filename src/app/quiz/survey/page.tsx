3a7cdacc44d900dd0957edef524c85e270ce1539
Victorio Nikolaev <vikinik01@abv.bg>
2025-04-25 15:19:55 +0300

    small change

--- src/app/quiz/survey/page.tsx
+++ src/app/quiz/survey/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { SurveyView } from "@/components/view/SurveyView";
+import { __IS_PROD__ } from "@/lib/consts";
 import { useRouter } from "next/navigation";
 import React from "react";
 
 function Page() {
    const router = useRouter();
+   if (__IS_PROD__) return null;
 
    return (
       <div className="mx-auto py-12">
