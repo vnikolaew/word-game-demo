7d2dad5f719cabb98dd77f766c7022acf8603e8d
Victorio Nikolaev <vikinik01@abv.bg>
2025-05-04 17:46:00 +0300

    x

---
+++ src/app/quiz/proficiency/ClientPage.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { match } from "ts-pattern";
+import React from "react";
+import { useProficiencyQuiz } from "./hooks";
+import InstructionsOne from "./InstructionsOne";
+import InstructionsTwo from "./InstructionsTwo";
+import QuizPage from "./QuizPage";
+
+function ClientPage() {
+   const { setState, state } = useProficiencyQuiz();
+
+   function handleNextPage(): void {
+      setState(`instructions-2`);
+   }
+
+   function handleStartQuiz(): void {
+      setState(`quiz`);
+   }
+
+   return match(state)
+      .with(`instructions`, (_) => <InstructionsOne onNext={handleNextPage} />)
+      .with(`instructions-2`, (_) => (
+         <InstructionsTwo onNext={handleStartQuiz} />
+      ))
+      .with(`quiz`, (_) => <QuizPage />)
+      .otherwise((_) => null);
+}
+
+export default ClientPage;
--- src/app/quiz/proficiency/page.tsx
+++ src/app/quiz/proficiency/page.tsx
@@ -1,31 +1,10 @@
-"use client";
 import React from "react";
-import { useProficiencyQuiz } from "./hooks";
-import InstructionsOne from "./InstructionsOne";
-import InstructionsTwo from "./InstructionsTwo";
-import QuizPage from "./QuizPage";
-import { match } from "ts-pattern";
+import ClientPage from "./ClientPage";
 
 export const dynamic = "force-dynamic";
 
-function Page() {
-   const { setState, state } = useProficiencyQuiz();
-
-   function handleNextPage(): void {
-      setState(`instructions-2`);
-   }
-
-   function handleStartQuiz(): void {
-      setState(`quiz`);
-   }
-
-   return match(state)
-      .with(`instructions`, (_) => <InstructionsOne onNext={handleNextPage} />)
-      .with(`instructions-2`, (_) => (
-         <InstructionsTwo onNext={handleStartQuiz} />
-      ))
-      .with(`quiz`, (_) => <QuizPage />)
-      .otherwise((_) => null);
+async function Page() {
+   return <ClientPage />;
 }
 
 export default Page;
