dab2575afb517b0ec4d932e33b9ca5e923066d6c
Victorio Nikolaev <vikinik01@abv.bg>
2025-05-06 11:42:21 +0300

    x

--- src/app/api/quiz/proficiency/route.ts
+++ src/app/api/quiz/proficiency/route.ts
@@ -3,6 +3,8 @@ import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
    const session = await getServerSession(authOptions);
    if (!session?.user) {
