7078d622df7dbcd4cc1a18470f27be0f1597e46d
Victorio Nikolaev <vikinik01@abv.bg>
2025-05-08 16:08:40 +0300

    x

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -2,6 +2,7 @@ export const __IS_DEV__ = process.env.NODE_ENV === `development`;
 
 export const __IS_PROD__ = process.env.NODE_ENV === `production`;
 
-export const __IS_TEST__ = process.env.NODE_ENV === `test`;
+// @ts-ignore
+export const __IS_TEST__ = process.env.NODE_ENV === `testing`;
 
 export const APP_NAME = `kalimat-game-learner`;
