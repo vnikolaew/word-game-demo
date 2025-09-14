51ab7c9202c5b3ecc34d44a89402be6c4a3ac1f7
Victorio Nikolaev <vikinik01@abv.bg>
2025-09-14 16:09:22 +0300

    Changing High Score Quiz to 60 instead of 80.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -49,4 +49,4 @@ export const PROLIFIC_EMAIL_REGEX = /^[a-f0-9]{24,}@email\.prolific\.com$/;
 
 export const PROLIFIC_USER_IMAGE = `https://s101-recruiting.cdn.greenhouse.io/external_greenhouse_job_boards/logos/400/045/310/original/Prolific_Icon_Prolific_Blue.png?1689233226`;
 
-export const HIGH_SCORE_THRESHOLD = 80;
+export const HIGH_SCORE_THRESHOLD = 60;
